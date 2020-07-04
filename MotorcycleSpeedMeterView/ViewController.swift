db9533dc0deae4ec027e3414ba2cec996b60ea16
Thinh Le <thinhlefrs@gmail.com>
2020-07-04 17:12:24 +0700

    Speed Meter View

--- MotorcycleSpeedMeterView/ViewController.swift
+++ MotorcycleSpeedMeterView/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var speedMeterView: MotorcycleSpeedMeterView!
+    
+    var speed: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        speedMeterView.delegate = self
     }
 
 
+    @IBAction func didTapChangeValueButton(_ sender: Any) {
+        let randomSpeed = Int.random(in: -150..<150)
+        speed += CGFloat(randomSpeed)
+        if speed > speedMeterView.maxValue {
+            speed = speedMeterView.maxValue
+        }
+        if speed < 0 {
+            speed = 0
+        }
+        speedMeterView.updateValue(speed)
+    }
+}
+
+extension ViewController: SpeedMeterViewDelegate {
+    func speedMeterView(_ view: MotorcycleSpeedMeterView, didChange value: CGFloat) {
+        print("Current Value: \(value)")
+    }
 }
 
